c6ef36a78ddfe86f90d8fdbc30c0832122b2a2eb
jerrychen <jerry@gmail.com>
2022-10-12 20:12:01 +0800

    新增user功能跟新增urls.py指定

---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+# http://127.0.0.1:8000/user/register
+urlpatterns = [
+    path('register/', views.user_register),
+  
+]
