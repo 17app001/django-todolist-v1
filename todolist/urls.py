34e201cc77d31dbe952ecad61b17b1cb700d3b78
jerrychen <jerry@gmail.com>
2022-10-17 21:43:02 +0800

    完成todo資料表建立

--- todolist/urls.py
+++ todolist/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-# http://127.0.0.1:8000/user/register
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('user.urls')),
-    path('',include('todo.urls')),
-  
+    path('',include('todo.urls')),  
 ]
