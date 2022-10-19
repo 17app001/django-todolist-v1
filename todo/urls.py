ce93385b239b230cf1635d5fb5273609b1c0b4fc
jerrychen <jerry@gmail.com>
2022-10-19 21:44:49 +0800

    完成建立表單頁面跟儲存到資料表

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.todo,name='todo'),  
+    path('create/',views.createtodo,name='createtodo'),
     path('view/<int:id>',views.viewtodo,name='viewtodo')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.shortcuts import get_object_or_404
 from .models import Todo
+from .forms import TodoForm
 
+
+def createtodo(request):
+    form=TodoForm()
+ 
+    if request.method=='POST':
+        print(request.POST)
+        if request.user.is_authenticated:
+            form=TodoForm(request.POST)
+            todo=form.save(commit=False)
+            todo.user=request.user
+            todo.save()
+
+            return redirect('todo')
+
+    return render(request,'./todo/createtodo.html',{'form':form})
 
 # Create your views here.
 def todo(request):
