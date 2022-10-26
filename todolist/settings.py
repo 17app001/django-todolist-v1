955ac7626311a469a238ccbd47ac0062376e0db8
jerrychen <jerry@gmail.com>
2022-10-26 21:01:59 +0800

    完成靜態檔案蒐集

--- todolist/settings.py
+++ todolist/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2-7v9%#8xz%886uu=3+m+vc-!=9&c1@&0z#+8zvl6ursci$k8$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'todolist.urls'
@@ -86,7 +87,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 #     }
 # }
 
-# MYSQL版本
+#MYSQL版本
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -97,6 +98,19 @@ DATABASES = {
         'PORT':'3306'
     }
 }
+
+# AWS MYSQL版本
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'todolist',
+#         'USER':'admin',
+#         'PASSWORD':'12345678',
+#         'HOST':'database-1.cviktbifdeon.ap-northeast-1.rds.amazonaws.com',
+#         'PORT':'3306'
+#     }
+# }
+
 
 
 # Password validation
@@ -138,6 +152,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
